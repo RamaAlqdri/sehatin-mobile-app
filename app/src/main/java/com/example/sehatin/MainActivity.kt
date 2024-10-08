057c629ecc8f005630aecf938c570e814f70908c
RamaAlqdri <ramadhanialqadri12@gmail.com>
2024-10-09 00:33:55 +0800

    feat(ui): add color scheme, theme, and font styles

--- app/src/main/java/com/example/sehatin/MainActivity.kt
+++ app/src/main/java/com/example/sehatin/MainActivity.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column (){
-
-            }
 
         }
     }
