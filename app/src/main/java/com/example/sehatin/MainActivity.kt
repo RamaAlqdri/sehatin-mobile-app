374d243df74c41adaae4e0fec0b5c8f32830ea7a
RamaAlqdri <ramadhanialqadri12@gmail.com>
2024-10-08 20:57:52 +0800

    feat(app): update application icon

--- app/src/main/java/com/example/sehatin/MainActivity.kt
+++ app/src/main/java/com/example/sehatin/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.sehatin
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column {
 
+            }
 
         }
     }
