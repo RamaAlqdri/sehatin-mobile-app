590e1e9de208e3bd3f0d038be575b4ac99fd00b2
RamaAlqdri <ramadhanialqadri12@gmail.com>
2024-10-09 19:12:31 +0800

    chore: add view, viewmodel, and model folders
    - Created folder structure to separate UI, business logic, and data layers

--- app/src/main/java/com/example/sehatin/MainActivity.kt
+++ app/src/main/java/com/example/sehatin/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Text(text = "Hello")
 
         }
     }
